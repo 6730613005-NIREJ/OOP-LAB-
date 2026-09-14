ddfde9aa1653b9b9fb8b1f9fd31d241e0023c927
NirejBatrai <purampandey1234@gmail.com>
2026-09-14 16:16:37 +0700

    nil chanhge

--- lab0/HelloWorld.java
+++ lab0/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld{
         System.out.println("Hi it is me Mew");
         System.out.println("Hi it  is Nil");
         System.out.println("hpw are you nil");
+        System.out.println("i am good ");
     }
 }
